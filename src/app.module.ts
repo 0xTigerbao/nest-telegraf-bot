cac1cfe8cacaed9df78f874514fde1cfadd97c60
0xtigerbao <devrymto@gmail.com>
2023-12-01 15:34:18 +0700

    remove TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,38 +4,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { BotModule } from './bot/bot.module';
 import { CurrenciesModule } from './currencies/currencies.module';
-import { PostgresModule } from './postgres/postgres.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CurrenciesEntity } from './postgres/entities/currencies.entity';
-import { TelegramUser } from './postgres/entities/telegram_users.entity';
-import { CategoriesEntity } from './postgres/entities/categories.entity';
-import { CostsEntity } from './postgres/entities/costs.entity';
-import { BudgetsEntity } from './postgres/entities/budgets.entity';
 import { I18nTranslateModule } from './i18n/i18n.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     BotModule,
     CurrenciesModule,
-    PostgresModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_LOGIN,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_MAIN,
-      entities: [
-        CurrenciesEntity,
-        TelegramUser,
-        CategoriesEntity,
-        CostsEntity,
-        BudgetsEntity,
-      ],
-      synchronize: true,
-    }),
     I18nTranslateModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/postgres/queries/categories.ts
+++
@@ -1,8 +0,0 @@
-export const generate_categories = `INSERT INTO PUBLIC.CATEGORIES(CATEGORY, CATEGORY_EN) VALUES 
-('Аренда жилья', 'Rent'),
-('Еда', 'Food'),
-('Клининг', 'Cleaning'),
-('Расходники','Consumables'),
-('Стирка', 'Laundry'),
-('Вода', 'Water'), 
-('Сотовая связь', 'Mobile communication') ON CONFLICT DO NOTHING`;
